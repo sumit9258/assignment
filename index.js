a6dbb1eeccaee5f1beb3aba87cfb53874c38692b
sumit9258 <sumitkumar925878@gmail.com>
2025-01-21 20:31:11 +0530

    task

---
+++ index.js
@@ -0,0 +1,92 @@
+// let a=50;
+
+// if (a>=90) {
+//     console.log("Grade A");
+    
+// }else if (a>80 && a<=89) {
+//     console.log("Grade B");
+    
+// } else if (a>=70 && a<=79) {
+//     console.log("Grade c");
+    
+// } else if (a>=50 && a<70) {
+//     console.log("Grade D");
+    
+// } else{
+//     console.log("Fail");
+    
+// }
+
+
+
+// let a = 4;
+// let b = 4;
+// let c = 0;
+// if (0 <= a && 0 <= b && 0 <= c) {
+
+
+//     if (a == b && b == a && c == a) {
+//         console.log("Equilateral triangle");
+
+//     }else  if (a == b && b == a && 0 < c || b == c && c == b && 0 < a || c == a && a == c && 0 < b) {
+//         console.log("Isosceles triangle");
+
+//     }else {
+//         console.log("Scalene triangle");
+
+//     }
+// } else {
+//     console.log("Invalid");
+
+// }
+
+
+// let a=73;
+
+// if (a%2==0) {
+//     if (a>=50) {
+//         console.log("even or large");
+        
+
+//     }else{
+//         console.log("even or small");
+        
+//     }
+
+// }else{
+// if(a<=30){
+//     console.log("odd or small");
+    
+// }else{
+//     console.log("odd or big");
+    
+// }
+// }
+
+
+// for (let i = 1; i <=10 ; i++) {
+//     let a=5*i
+//     console.log(a);
+    
+// }
+
+// let result=0;
+// let b=6;
+// for (let i = 0; i <= b; i++) {
+// result +=i    
+    
+// }
+// console.log(result);
+
+
+// for (let i = 1; i < 5; i++) {
+//     let b="";
+//     for (let j = 1; j <= i; j++) {
+//         b +=j
+        
+        
+//     }
+//     console.log(b);
+    
+// }
+
