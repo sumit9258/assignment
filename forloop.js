7a02a9050a0fbdbf15f0f1823501aac5bfdff4a2
sumit9258 <sumitkumar925878@gmail.com>
2025-01-22 21:23:55 +0530

    for loop questions

---
+++ forloop.js
@@ -0,0 +1,95 @@
+let a=5;
+for (let i = 0; i < a; i++) {
+    let a="";
+    for (let j = 0; j <= i; j++) {     // question 1
+       a +="*" 
+    }
+    console.log(a);
+    
+}
+
+
+// let a=5;
+
+// for (let i = a; i > 0; i--) {                         //2
+//     let b="";
+//     for (let j = 0; j < i; j++) {
+//         b +="*"
+//     }
+// console.log(b);
+
+// }
+
+// let a=5;
+// for (let i = 0; i < a; i++) {                          //3
+//     let b="";
+//     for (let j = 0; j <a- i; j++) {
+//         b +=" "
+//     }
+//     for (let j = 0; j <=2*i; j++) {
+//         b +="*"
+//     }
+//     console.log(b);
+    
+// }
+
+
+// let a=5;
+
+// for (let i = 1; i <= 5; i++) {                          //4
+//     let b="";
+//     for (let j = 1; j <=i; j++) {
+//         b +=j
+
+//     }
+//     console.log(b);
+    
+// }
+
+
+
+// let a=5;
+
+// for (let i = 0; i <a; i++) {         //5
+//     let b="";
+//     for (let j = 5; j > i; j--) {
+//         b +=j
+//     }
+//     console.log(b);
+    
+// }
+
+
+
+// let a=4;
+                                                   //6
+// for (let i = 0; i <= a; i++) {                         
+//     let b="";
+//     for (let j = 0; j <=a- i; j++) {
+//         b +=" "
+//     }
+//     for (let j = 0; j <=2*i; j++) {
+//         b +="*"
+//     }
+//     console.log(b);
+    
+// }
+
+
+// for (let i = a-1; i >=0; i--) {
+//     let b="";
+//     for (let k = 0; k <=a- i; k++) {
+//         b+=" ";
+//     }
+//     for (let j =i*2; j >=0; j--) {
+//         b +="*"
+//     }
+//     console.log(b);
+    
+
+// }
+
+
+
+
+
