cc5823a10b43afde520c69596cf8f63cdda18bf6
sumit9258 <sumitkumar925878@gmail.com>
2025-01-23 22:18:19 +0530

    lastdayforloop

---
+++ lastdayforloop.js
@@ -0,0 +1,116 @@
+// let a=10
+// for (let i = 1; i <= a; i++) {           //1
+//     console.log(i);
+    
+// }
+
+
+
+// let a=20;
+// for (let i = 1; i <= a; i++) {                        //2
+//     if (i%2!==0) {
+        
+//         console.log(i);
+//     }
+// }
+
+
+// let a=5;
+
+// for (let i = 1; i <= a; i++) {                          //3
+//     console.log(i**2);
+    
+// }
+
+
+// let a=100;
+
+// for (let i = 1; i <= a; i++) {                                       //4
+//     if (i%5==0) {
+//         console.log(i);
+        
+//     }
+// }
+
+
+
+
+// let a=1;
+// let b=1;
+// console.log(a);                         //  5
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(b);
+    
+//     let c=a+b;
+//     a=b;
+//     b=c;
+
+// }
+
+
+// let a=5;
+
+// for (let i = 1; i <=a; i++) {                            //6
+//     let b="";
+//     for (let j = 0; j < i; j++) {
+//         b +="*"
+//     }
+// console.log(b);
+
+// }
+
+
+// let a=7;
+
+// for (let i = 1; i <=10; i++) {                               //7
+//     console.log(a,"x",i,"=",i*a);
+    
+// }
+
+
+// let a="javascript";
+
+// for (let i = 0; i < a.length; i++) {                       //8
+//     console.log(a[i]);
+    
+// }
+
+
+
+
+// let a=50;
+// let b=0
+// for (let i = 1; i <= a; i++) {                                     //9
+//     if (i%2==0) {
+//         b +=i;
+//     }
+    
+// }
+// console.log(b);
+
+
+
+
+
+// let a=3;
+
+// for (let i = 1; i <= a; i++) {                             //10
+//     let b="";
+//     for (let j = 1; j <=a-i ; j++) {
+//     b+=" ";
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         b +=j
+//     }
+//     for (let j = i-1; j>=1; j--) {
+//         b +=j
+//     }
+// console.log(b);
+
+
+// }
+
+
+
